203ca0184b0f3a72a3fedd11a70ce984cb5458fa
Dina Nashaat <dina.nashaat@gmail.com>
2019-07-12 06:21:38 +0200

    Add routes for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
+  resources :chats, param: :number do
+    resources :messages, param: :number do
+      collection do
+        get :search
+      end
+    end
+  end
+  resources :applications, except: :update
+
+  put '/applications', to: 'applications#update'
+  patch '/applications', to: 'applications#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
