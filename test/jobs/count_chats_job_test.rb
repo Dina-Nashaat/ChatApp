3214e2523c3ca483b767089665b24d02f08485b1
Dina Nashaat <dina.nashaat@gmail.com>
2019-07-12 06:22:37 +0200

    Add default tests

---
+++ test/jobs/count_chats_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CountChatsJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/jobs/persist_chat_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PersistChatJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
